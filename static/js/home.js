0310ee46d1e9b9d920f89e0bbb27032d8cdad848
beru26 <berkes.edina@gmail.com>
2016-09-27 14:59:01 +0200

    more example added

--- static/js/home.js
+++ static/js/home.js
@@ -7,7 +7,9 @@ var Boards = function(title, text)
 var board1 = new Boards("title1", "text1")
 var board2 = new Boards("title2", "text2")
 var board3 = new Boards("title3", "text3")
-var a = [board1, board2, board3]
+var board4 = new Boards("title2", "text2")
+var board5 = new Boards("title3", "text3")
+var a = [board1, board2, board3, board4, board5]
 localStorage.boards = JSON.stringify(a)
 
 var test = function()
