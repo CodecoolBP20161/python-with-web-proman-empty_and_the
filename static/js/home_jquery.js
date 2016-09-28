31c1dbc15d1ce5809b8ab7550e48ff28bc1504b8
vbenedek <xbenedek01@gmail.com>
2016-09-28 22:01:47 +0200

    fadein instead of show

--- static/js/home_jquery.js
+++ static/js/home_jquery.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
     $("#cancel").click(function(){
         $(".newForm").hide();
-        $("#addbutton").show();
+        $("#addbutton").fadeIn();
         $('#title').val('');
         $('#text').val('');
     });
