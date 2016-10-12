305f5de0440ba39ea4ad396505d616a70f4d70a8
vbenedek <xbenedek01@gmail.com>
2016-10-12 21:49:56 +0200

    wrong argument corrected

--- refactoring/build.py
+++ refactoring/build.py
@@ -7,4 +7,4 @@ db.create_tables([Board, Card], safe=True)
 
 for i in range(10):
     board = Board.create(title="board" + str(i), body="boardbody" + str(i))
-    card = Card.create(title="card" + str(i), body="cardbody" + str(i), body_id=board)
+    card = Card.create(title="card" + str(i), body="cardbody" + str(i), board_id=board)
