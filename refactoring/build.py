6598ebcdc8ad36d41c8c980ebfbdd413b5fcfabf
vbenedek <xbenedek01@gmail.com>
2016-10-12 21:46:01 +0200

    build tables and put some example data in them

---
+++ refactoring/build.py
@@ -0,0 +1,10 @@
+from model import *
+
+db.connect()
+
+db.drop_tables([Board, Card], safe=True, cascade=True)
+db.create_tables([Board, Card], safe=True)
+
+for i in range(10):
+    board = Board.create(title="board" + str(i), body="boardbody" + str(i))
+    card = Card.create(title="card" + str(i), body="cardbody" + str(i), body_id=board)
