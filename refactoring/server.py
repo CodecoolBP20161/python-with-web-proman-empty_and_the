6f1976558a84640fc4b9b1c5b17b94b04f352536
vbenedek <xbenedek01@gmail.com>
2016-10-13 12:03:44 +0200

    handle not existing data

--- refactoring/server.py
+++ refactoring/server.py
@@ -14,23 +14,30 @@ def index():
 def all_boards():
     if request.method == 'GET':
         boards = Board.get_dict_from_table()
+        if not boards:
+            boards = {}
         return jsonify(boards)
 
 
 @app.route('/api/boards/<board_id>', methods=['GET', 'POST'])
 def board(board_id):
     if request.method == 'GET':
-        board = Board.select().where(Board.id==board_id).get()
-        board_dict = board.get_dict_from_object()
-        return jsonify(board_dict)
+        try:
+            board = Board.select().where(Board.id == board_id).get()
+            board_dict = board.get_dict_from_object()
+        except:
+            board_dict = {}
+        finally:
+            return jsonify(board_dict)
 
 
 @app.route('/api/boards/<board_id>/cards', methods=['GET', 'POST'])
 def all_cards(board_id):
     if request.method == 'GET':
         cards = Card.get_dict_from_table(board_id)
+        if not cards["cards"]:
+            cards = {}
         return jsonify(cards)
-
 
 
 if __name__ == "__main__":
