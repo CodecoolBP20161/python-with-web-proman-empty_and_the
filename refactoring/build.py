6fe84cc3c3f4c5d4c39e0d2c3511f078c796df85
vbenedek <xbenedek01@gmail.com>
2016-10-13 14:09:03 +0200

    example data not generated

--- refactoring/build.py
+++ refactoring/build.py
@@ -5,8 +5,8 @@ db.connect()
 db.drop_tables([Board, Card], safe=True, cascade=True)
 db.create_tables([Board, Card], safe=True)
 
-for i in range(10):
-    board = Board.create(title="board" + str(i), body="boardbody" + str(i))
-    card = Card.create(title="card" + str(i), body="cardbody" + str(i), board_id=board)
-    card = Card.create(title="card1" + str(i), body="cardbody1" + str(i), board_id=board)
-    card = Card.create(title="card2" + str(i), body="cardbody2" + str(i), board_id=board)
+# for i in range(10):
+#     board = Board.create(title="board" + str(i), body="boardbody" + str(i))
+#     card = Card.create(title="card" + str(i), body="cardbody" + str(i), board_id=board)
+#     card = Card.create(title="card1" + str(i), body="cardbody1" + str(i), board_id=board)
+#     card = Card.create(title="card2" + str(i), body="cardbody2" + str(i), board_id=board)
