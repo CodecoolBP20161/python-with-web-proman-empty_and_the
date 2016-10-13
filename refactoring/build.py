75782b439df04ec07b958a849474c0e0bc2d6fed
vbenedek <xbenedek01@gmail.com>
2016-10-13 11:39:17 +0200

    more example data for testing

--- refactoring/build.py
+++ refactoring/build.py
@@ -8,3 +8,5 @@ db.create_tables([Board, Card], safe=True)
 for i in range(10):
     board = Board.create(title="board" + str(i), body="boardbody" + str(i))
     card = Card.create(title="card" + str(i), body="cardbody" + str(i), board_id=board)
+    card = Card.create(title="card1" + str(i), body="cardbody1" + str(i), board_id=board)
+    card = Card.create(title="card2" + str(i), body="cardbody2" + str(i), board_id=board)
