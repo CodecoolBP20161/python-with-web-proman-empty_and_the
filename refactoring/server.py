1c4037f61ad50ed959456dd2cd9a8a835df35022
vbenedek <xbenedek01@gmail.com>
2016-10-13 11:39:37 +0200

    api GET routes for boards and cards

--- refactoring/server.py
+++ refactoring/server.py
@@ -1,12 +1,36 @@
 from flask import *
+from model import Board, Card
+
 
 app = Flask(__name__)
 
 
-# server
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/api/boards', methods=['GET', 'POST'])
+def all_boards():
+    if request.method == 'GET':
+        boards = Board.get_dict_from_table()
+        return jsonify(boards)
+
+
+@app.route('/api/boards/<board_id>', methods=['GET', 'POST'])
+def board(board_id):
+    if request.method == 'GET':
+        board = Board.select().where(Board.id==board_id).get()
+        board_dict = board.get_dict_from_object()
+        return jsonify(board_dict)
+
+
+@app.route('/api/boards/<board_id>/cards', methods=['GET', 'POST'])
+def all_cards(board_id):
+    if request.method == 'GET':
+        cards = Card.get_dict_from_table(board_id)
+        return jsonify(cards)
+
 
 
 if __name__ == "__main__":
