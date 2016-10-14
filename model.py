18a1b125fae664b6a8d8bc2a4e75779c85361682
vbenedek <xbenedek01@gmail.com>
2016-10-14 15:33:33 +0200

    dbname, dbuser

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('vbenedek', user='vbenedek')
+db = PostgresqlDatabase('dbname', user='dbuser')
 
 
 class BaseModel(Model):
